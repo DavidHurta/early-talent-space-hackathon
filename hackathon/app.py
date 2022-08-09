f0a4a8fe229ef8cc72f825f1aa97ec0edb4b36fc
David Hurta <dhurta@redhat.com>
2022-08-09 18:16:02 +0200

    Add if __name__ == __main__:

--- hackathon/app.py
+++ hackathon/app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 from kafka import KafkaConsumer
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
 @app.route("/")
 def index():
     return render_template('index.html')
