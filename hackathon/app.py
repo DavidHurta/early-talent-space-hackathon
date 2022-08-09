7990333b9e6a072a2a525615053613d20baad6be
David Hurta <dhurta@redhat.com>
2022-08-09 18:38:59 +0200

    Remove the if __name__ because it's redundant

--- hackathon/app.py
+++ hackathon/app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template
 from kafka import KafkaConsumer
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
 
 @app.route("/")
 def index():
